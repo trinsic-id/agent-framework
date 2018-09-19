d7d4295ceca6e3e6fab8f70e13e9668e079ca897
Tomislav Markovski <tmarkovski@gmail.com>
2018-09-19 09:32:30 -0400

    Moved connection establish and credential issue to Scenarios for reuse

--- src/Streetcred.Sdk.Tests/ConnectionTests.cs
+++ src/Streetcred.Sdk.Tests/ConnectionTests.cs
@@ -42,7 +42,7 @@ namespace Streetcred.Sdk.Tests
             provisioningMock.Setup(x => x.GetProvisioningAsync(It.IsAny<Wallet>()))
                 .Returns(Task.FromResult(new ProvisioningRecord
                 {
-                    Endpoint = new AgentEndpoint {Uri = MockEndpointUri}
+                    Endpoint = new AgentEndpoint { Uri = MockEndpointUri }
                 }));
 
             _connectionService = new ConnectionService(
@@ -76,7 +76,7 @@ namespace Streetcred.Sdk.Tests
             var connectionId = Guid.NewGuid().ToString();
 
             var invitation = await _connectionService.CreateInvitationAsync(_issuerWallet,
-                new CreateInviteConfiguration() {ConnectionId = connectionId});
+                new CreateInviteConfiguration() { ConnectionId = connectionId });
 
             var connection = await _connectionService.GetAsync(_issuerWallet, connectionId);
 
@@ -87,76 +87,8 @@ namespace Streetcred.Sdk.Tests
         [Fact]
         public async Task CanEstablishConnectionAsync()
         {
-            // Create invitation by the issuer
-            var issuerConnectionId = Guid.NewGuid().ToString();
-
-            var inviteConfig = new CreateInviteConfiguration()
-            {
-                ConnectionId = issuerConnectionId,
-                MyAlias = new ConnectionAlias()
-                {
-                    Name = "Issuer",
-                    ImageUrl = "www.issuerdomain.com/profilephoto"
-                },
-                TheirAlias = new ConnectionAlias()
-                {
-                    Name = "Holder",
-                    ImageUrl = "www.holderdomain.com/profilephoto"
-                }
-            };
-
-            // Issuer creates an invitation
-            var invitation = await _connectionService.CreateInvitationAsync(_issuerWallet, inviteConfig);
-
-            var connectionIssuer = await _connectionService.GetAsync(_issuerWallet, issuerConnectionId);
-
-            Assert.Equal(ConnectionState.Invited, connectionIssuer.State);
-            Assert.True(invitation.Name == inviteConfig.MyAlias.Name &&
-                        invitation.ImageUrl == inviteConfig.MyAlias.ImageUrl);
-
-            // Holder accepts invitation and sends a message request
-            var holderConnectionId = await _connectionService.AcceptInvitationAsync(_holderWallet, invitation);
-            var connectionHolder = await _connectionService.GetAsync(_holderWallet, holderConnectionId);
-
-            Assert.Equal(ConnectionState.Negotiating, connectionHolder.State);
-
-            // Issuer processes incoming message
-            var issuerMessage = _messages.OfType<ForwardToKeyEnvelopeMessage>()
-                .First(x => x.Type.Contains(connectionIssuer.Tags.Single(item => item.Key == "connectionKey").Value));
-
-            var requestMessage = GetContentMessage(issuerMessage) as ConnectionRequest;
-            Assert.NotNull(requestMessage);
-
-            // Issuer stores the connection request
-            await _connectionService.StoreRequestAsync(_issuerWallet, requestMessage);
-
-            connectionIssuer = await _connectionService.GetAsync(_issuerWallet, issuerConnectionId);
-            Assert.Equal(ConnectionState.Negotiating, connectionIssuer.State);
-
-            // Issuer accepts the connection request
-            await _connectionService.AcceptRequestAsync(_issuerWallet, issuerConnectionId);
-
-            connectionIssuer = await _connectionService.GetAsync(_issuerWallet, issuerConnectionId);
-            Assert.Equal(ConnectionState.Connected, connectionIssuer.State);
-
-            // Holder processes incoming message
-            var holderMessage = _messages.OfType<ForwardEnvelopeMessage>()
-                .First(x => x.Type.Contains(connectionHolder.MyDid));
-
-            var responseMessage = GetContentMessage(holderMessage) as ConnectionResponse;
-            Assert.NotNull(responseMessage);
-
-            // Holder accepts response message
-            await _connectionService.AcceptResponseAsync(_holderWallet, responseMessage);
-
-            // Retrieve updated connection state for both issuer and holder
-            connectionIssuer = await _connectionService.GetAsync(_issuerWallet, issuerConnectionId);
-            connectionHolder = await _connectionService.GetAsync(_holderWallet, holderConnectionId);
-
-            Assert.True(connectionIssuer.Alias.Name == inviteConfig.TheirAlias.Name &&
-                        connectionIssuer.Alias.ImageUrl == inviteConfig.TheirAlias.ImageUrl);
-            Assert.True(connectionHolder.Alias.Name == inviteConfig.MyAlias.Name &&
-                        connectionHolder.Alias.ImageUrl == inviteConfig.MyAlias.ImageUrl);
+            var (connectionIssuer, connectionHolder) = await Scenarios.EstablishConnectionAsync(
+                _connectionService, _messages, _issuerWallet, _holderWallet);
 
             Assert.Equal(ConnectionState.Connected, connectionIssuer.State);
             Assert.Equal(ConnectionState.Connected, connectionHolder.State);
@@ -168,7 +100,7 @@ namespace Streetcred.Sdk.Tests
             Assert.Equal(connectionIssuer.Endpoint.Uri, MockEndpointUri);
         }
 
-        private IContentMessage GetContentMessage(IEnvelopeMessage message)
+        private static IContentMessage GetContentMessage(IEnvelopeMessage message)
             => JsonConvert.DeserializeObject<IContentMessage>(message.Content);
 
         public async Task DisposeAsync()
---
+++ src/Streetcred.Sdk.Tests/Scenarios.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Hyperledger.Indy.AnonCredsApi;
+using Hyperledger.Indy.DidApi;
+using Hyperledger.Indy.PoolApi;
+using Hyperledger.Indy.WalletApi;
+using Newtonsoft.Json;
+using Streetcred.Sdk.Contracts;
+using Streetcred.Sdk.Model;
+using Streetcred.Sdk.Model.Connections;
+using Streetcred.Sdk.Model.Credentials;
+using Streetcred.Sdk.Model.Records;
+using Xunit;
+using Streetcred.Sdk.Utils;
+
+namespace Streetcred.Sdk.Tests
+{
+    internal static class Scenarios
+    {
+
+        internal static async Task<(ConnectionRecord firstParty, ConnectionRecord secondParty)> EstablishConnectionAsync(
+            IConnectionService connectionService,
+            IProducerConsumerCollection<IEnvelopeMessage> _messages,
+            Wallet firstWallet,
+            Wallet secondWallet)
+        {
+            // Create invitation by the issuer
+            var issuerConnectionId = Guid.NewGuid().ToString();
+
+            var inviteConfig = new CreateInviteConfiguration()
+            {
+                ConnectionId = issuerConnectionId,
+                MyAlias = new ConnectionAlias()
+                {
+                    Name = "Issuer",
+                    ImageUrl = "www.issuerdomain.com/profilephoto"
+                },
+                TheirAlias = new ConnectionAlias()
+                {
+                    Name = "Holder",
+                    ImageUrl = "www.holderdomain.com/profilephoto"
+                }
+            };
+
+            // Issuer creates an invitation
+            var invitation = await connectionService.CreateInvitationAsync(firstWallet, inviteConfig);
+
+            var connectionIssuer = await connectionService.GetAsync(firstWallet, issuerConnectionId);
+
+            Assert.Equal(ConnectionState.Invited, connectionIssuer.State);
+            Assert.True(invitation.Name == inviteConfig.MyAlias.Name &&
+                        invitation.ImageUrl == inviteConfig.MyAlias.ImageUrl);
+
+            // Holder accepts invitation and sends a message request
+            var holderConnectionId = await connectionService.AcceptInvitationAsync(secondWallet, invitation);
+            var connectionHolder = await connectionService.GetAsync(secondWallet, holderConnectionId);
+
+            Assert.Equal(ConnectionState.Negotiating, connectionHolder.State);
+
+            // Issuer processes incoming message
+            var issuerMessage = _messages.OfType<ForwardToKeyEnvelopeMessage>()
+                .First(x => x.Type.Contains(connectionIssuer.Tags.Single(item => item.Key == "connectionKey").Value));
+
+            var requestMessage = GetContentMessage(issuerMessage) as ConnectionRequest;
+            Assert.NotNull(requestMessage);
+
+            // Issuer stores the connection request
+            await connectionService.StoreRequestAsync(firstWallet, requestMessage);
+
+            connectionIssuer = await connectionService.GetAsync(firstWallet, issuerConnectionId);
+            Assert.Equal(ConnectionState.Negotiating, connectionIssuer.State);
+
+            // Issuer accepts the connection request
+            await connectionService.AcceptRequestAsync(firstWallet, issuerConnectionId);
+
+            connectionIssuer = await connectionService.GetAsync(firstWallet, issuerConnectionId);
+            Assert.Equal(ConnectionState.Connected, connectionIssuer.State);
+
+            // Holder processes incoming message
+            var holderMessage = _messages.OfType<ForwardEnvelopeMessage>()
+                .First(x => x.Type.Contains(connectionHolder.MyDid));
+
+            var responseMessage = GetContentMessage(holderMessage) as ConnectionResponse;
+            Assert.NotNull(responseMessage);
+
+            // Holder accepts response message
+            await connectionService.AcceptResponseAsync(secondWallet, responseMessage);
+
+
+            // Retrieve updated connection state for both issuer and holder
+            connectionIssuer = await connectionService.GetAsync(firstWallet, issuerConnectionId);
+            connectionHolder = await connectionService.GetAsync(secondWallet, holderConnectionId);
+
+            Assert.True(connectionIssuer.Alias.Name == inviteConfig.TheirAlias.Name &&
+                        connectionIssuer.Alias.ImageUrl == inviteConfig.TheirAlias.ImageUrl);
+            Assert.True(connectionHolder.Alias.Name == inviteConfig.MyAlias.Name &&
+                        connectionHolder.Alias.ImageUrl == inviteConfig.MyAlias.ImageUrl);
+
+            return (connectionIssuer, connectionHolder);
+        }
+
+
+        internal static async Task<(CredentialRecord issuerCredential, CredentialRecord holderCredential)> IssueCredentialAsync(
+            ISchemaService schemaService, ICredentialService credentialService,
+            IProducerConsumerCollection<IEnvelopeMessage> messages,
+            string issuerConnectionId, Wallet issuerWallet, Wallet holderWallet,
+            Pool pool, string proverMasterSecretId)
+        {
+
+            // Create an issuer DID/VK. Can also be created during provisioning
+            var issuer = await Did.CreateAndStoreMyDidAsync(issuerWallet,
+                new { seed = "000000000000000000000000Steward1" }.ToJson());
+
+            // Creata a schema and credential definition for this issuer
+            var schemaId = await schemaService.CreateSchemaAsync(pool, issuerWallet, issuer.Did,
+                $"Test-Schema-{Guid.NewGuid().ToString()}", "1.0", new[] { "first_name", "last_name" });
+            var definitionId =
+                await schemaService.CreateCredentialDefinitionAsync(pool, issuerWallet, schemaId, issuer.Did, true,
+                    100);
+
+            // Send an offer to the holder using the established connection channel
+            await credentialService.SendOfferAsync(definitionId, issuerConnectionId, issuerWallet, issuer.Did);
+
+            // Holder retrives message from their cloud agent
+            var credentialOffer = FindContentMessage<CredentialOffer>(messages);
+
+            // Holder stores the credential offer
+            var holderCredentialId =
+                await credentialService.StoreOfferAsync(holderWallet, credentialOffer);
+
+            // Holder creates master secret. Will also be created during wallet agent provisioning
+            await AnonCreds.ProverCreateMasterSecretAsync(holderWallet, proverMasterSecretId);
+
+            // Holder accepts the credential offer and sends a credential request
+            await credentialService.AcceptOfferAsync(holderWallet, pool, holderCredentialId,
+                new Dictionary<string, string>
+                {
+                    {"first_name", "Jane"},
+                    {"last_name", "Doe"}
+                });
+
+            // Issuer retrieves credential request from cloud agent
+            var credentialRequest = FindContentMessage<CredentialRequest>(messages);
+            Assert.NotNull(credentialRequest);
+
+            // Issuer stores the credential request
+            var issuerCredentialId =
+                await credentialService.StoreCredentialRequestAsync(issuerWallet, credentialRequest);
+
+            // Issuer accepts the credential requests and issues a credential
+            await credentialService.IssueCredentialAsync(pool, issuerWallet, issuer.Did, issuerCredentialId);
+
+            // Holder retrieves the credential from their cloud agent
+            var credential = FindContentMessage<Credential>(messages);
+            Assert.NotNull(credential);
+
+            // Holder stores the credential in their wallet
+            await credentialService.StoreCredentialAsync(pool, holderWallet, credential);
+
+            // Verify states of both credential records are set to 'Issued'
+            var issuerCredential = await credentialService.GetAsync(issuerWallet, issuerCredentialId);
+            var holderCredential = await credentialService.GetAsync(holderWallet, holderCredentialId);
+
+            return (issuerCredential, holderCredential);
+        }
+
+        static IContentMessage GetContentMessage(IEnvelopeMessage message)
+            => JsonConvert.DeserializeObject<IContentMessage>(message.Content);
+
+        private static T FindContentMessage<T>(IEnumerable<IEnvelopeMessage> collection)
+            where T : IContentMessage
+            => collection.Select(GetContentMessage).OfType<T>().Single();
+    }
+}
