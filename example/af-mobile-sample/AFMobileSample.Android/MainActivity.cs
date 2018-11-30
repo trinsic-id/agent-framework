41af4e377bccbd618a2aaaf5beb54f1b8c5d1fe1
Tomislav Markovski <tmarkovski@gmail.com>
2018-11-30 14:11:46 -0500

    Xamarin Forms samples for iOS and Android

---
+++ example/af-mobile-sample/AFMobileSample.Android/MainActivity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Java.Lang;
+
+namespace AFMobileSample.Droid
+{
+    [Activity(Label = "AFMobileSample", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            JavaSystem.LoadLibrary("gnustl_shared");
+            JavaSystem.LoadLibrary("indy");
+
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+    }
+}
