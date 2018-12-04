2b5928dba8b6966f1ade7a28cb58b0cd8326ff88
Tomislav Markovski <tmarkovski@gmail.com>
2018-12-04 18:35:24 -0500

    Xamarin Mobile Samples (#68)
    * Xamarin Forms samples for iOS and Android
    
    * Remove examples from the framework solution
    
    * Docker update for web agents. Directory rename.
    
    * Progress indicator. Android permission fix.
    
    * iOS documentation, initial
    
    * Xamarin.Android documentation updates
    
    * Readme updates
    
    * Add details for Xamarin

---
+++ samples/xamarin-mobile-sample/AFMobileSample/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using AgentFramework.Core.Contracts;
+using AgentFramework.Core.Models.Wallets;
+using Autofac;
+using Hyperledger.Indy.DidApi;
+using Hyperledger.Indy.WalletApi;
+using Xamarin.Forms;
+
+namespace AFMobileSample
+{
+    public partial class MainPage : ContentPage
+    {
+        private readonly IWalletService _walletService = App.Container.Resolve<IWalletService>();
+        private readonly IConnectionService _connectionService = App.Container.Resolve<IConnectionService>();
+
+        private WalletConfiguration _config = new WalletConfiguration { Id = "MyWallet" };
+        private WalletCredentials _creds = new WalletCredentials { Key = "SecretKey" };
+        private Wallet _wallet;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnCreateClicked(object sender, EventArgs e)
+        {
+            ProgressIndicator.IsRunning = true;
+            CreateButton.IsEnabled = false;
+
+            try
+            {
+                await _walletService.CreateWalletAsync(_config, _creds);
+            }
+            catch (WalletExistsException)
+            {
+                //
+            }
+
+            _wallet = _wallet ?? await _walletService.GetWalletAsync(_config, _creds);
+            var did = await Did.CreateAndStoreMyDidAsync(_wallet, "{}");
+
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                DidLabel.Text = $"Identity created -> Did: {did.Did}";
+
+                ProgressIndicator.IsRunning = false;
+                CreateButton.IsEnabled = true;
+            });
+        }
+    }
+}
