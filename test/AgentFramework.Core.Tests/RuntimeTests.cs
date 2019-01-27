2ce663b1b12649042af363d161733af4c5875643
Tobias Looker <tplooker@gmail.com>
2019-01-28 08:34:55 +1300

    AspNetCore Unit Test Suite (#98)
    * Test suite for the aspnet added, baseline tests for the extensions.
    
    * Merged the tests suite into a single project due to an un-resolved issue with dotnet testing execution from solutions.

--- test/AgentFramework.Core.Tests/RuntimeTests.cs
+++ test/AgentFramework.Core.Tests/RuntimeTests.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Net.Http;
-using System.Text;
+﻿using System.Net.Http;
 using AgentFramework.Core.Contracts;
 using Autofac;
 using Autofac.Extensions.DependencyInjection;
