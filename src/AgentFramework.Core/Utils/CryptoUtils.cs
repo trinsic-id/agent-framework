18e80016fda2d16dc59be0e510a512c37b3e0026
Tomislav Markovski <tmarkovski@gmail.com>
2019-01-29 15:15:24 -0500

    Comments

--- src/AgentFramework.Core/Utils/CryptoUtils.cs
+++ src/AgentFramework.Core/Utils/CryptoUtils.cs
@@ -16,7 +16,7 @@ namespace AgentFramework.Core.Utils
         /// <param name="recipientKey">The recipient key.</param>
         /// <param name="senderKey">The sender key.</param>
         /// <param name="message">The message.</param>
-        /// <returns></returns>
+        /// <returns>Encrypted message formatted as JWE using UTF8 byte order</returns>
         public static Task<byte[]> PackAsync(
             Wallet wallet, string recipientKey, string senderKey, byte[] message) =>
             PackAsync(wallet, new[] {recipientKey}, senderKey, message);
@@ -26,7 +26,7 @@ namespace AgentFramework.Core.Utils
         /// <param name="recipientKeys">The recipient keys.</param>
         /// <param name="senderKey">The sender key.</param>
         /// <param name="message">The message.</param>
-        /// <returns></returns>
+        /// <returns>Encrypted message formatted as JWE using UTF8 byte order</returns>
         public static Task<byte[]> PackAsync(
             Wallet wallet, string[] recipientKeys, string senderKey, byte[] message) =>
             Crypto.PackMessageAsync(wallet, recipientKeys.ToJson(), senderKey, message);
@@ -37,7 +37,7 @@ namespace AgentFramework.Core.Utils
         /// <param name="recipientKey">The recipient key.</param>
         /// <param name="senderKey">The sender key.</param>
         /// <param name="message">The message.</param>
-        /// <returns></returns>
+        /// <returns>Encrypted message formatted as JWE using UTF8 byte order</returns>
         public static Task<byte[]> PackAsync<T>(
             Wallet wallet, string recipientKey, string senderKey, T message) =>
             PackAsync(wallet, new[] {recipientKey}, senderKey, message.ToByteArray());
@@ -48,7 +48,7 @@ namespace AgentFramework.Core.Utils
         /// <param name="recipientKeys">The recipient keys.</param>
         /// <param name="senderKey">The sender key.</param>
         /// <param name="message">The message.</param>
-        /// <returns></returns>
+        /// <returns>Encrypted message formatted as JWE using UTF8 byte order</returns>
         public static Task<byte[]> PackAsync<T>(
             Wallet wallet, string[] recipientKeys, string senderKey, T message) =>
             Crypto.PackMessageAsync(wallet, recipientKeys.ToJson(), senderKey, message.ToByteArray());
@@ -56,7 +56,7 @@ namespace AgentFramework.Core.Utils
         /// <summary>Unpacks the asynchronous.</summary>
         /// <param name="wallet">The wallet.</param>
         /// <param name="message">The message.</param>
-        /// <returns></returns>
+        /// <returns>Decrypted message as UTF8 string and sender/recipient key information</returns>
         public static async Task<UnpackResult> UnpackAsync(Wallet wallet, byte[] message)
         {
             var result = await Crypto.UnpackMessageAsync(wallet, message);
@@ -67,7 +67,7 @@ namespace AgentFramework.Core.Utils
         /// <typeparam name="T"></typeparam>
         /// <param name="wallet">The wallet.</param>
         /// <param name="message">The message.</param>
-        /// <returns></returns>
+        /// <returns>Decrypted message as UTF8 string and sender/recipient key information</returns>
         public static async Task<T> UnpackAsync<T>(Wallet wallet, byte[] message)
         {
             var result = await Crypto.UnpackMessageAsync(wallet, message);
@@ -76,14 +76,23 @@ namespace AgentFramework.Core.Utils
         }
     }
 
-    public class UnpackResult
+    /// <summary>
+    /// Result object from <see cref="Crypto.UnpackMessageAsync"/>
+    /// </summary>
+    internal class UnpackResult
     {
+        /// <summary>Gets or sets the message encoded as UTF8 string.</summary>
+        /// <value>The message.</value>
         [JsonProperty("message")]
         public string Message { get; set; }
 
+        /// <summary>Gets or sets the sender verkey.</summary>
+        /// <value>The sender verkey.</value>
         [JsonProperty("sender_verkey")]
         public string SenderVerkey { get; set; }
 
+        /// <summary>Gets or sets the recipient verkey.</summary>
+        /// <value>The recipient verkey.</value>
         [JsonProperty("recipient_verkey")]
         public string RecipientVerkey { get; set; }
     }
