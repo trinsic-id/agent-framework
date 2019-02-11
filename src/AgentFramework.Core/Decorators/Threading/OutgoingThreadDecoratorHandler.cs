1054a81b56d553056cec4cdb194d9b1f87bc9095
Tobias Looker <tplooker@gmail.com>
2019-02-11 15:44:09 +1300

    Adding outgoing threading decorator support.

---
+++ src/AgentFramework.Core/Decorators/Threading/OutgoingThreadDecoratorHandler.cs
@@ -0,0 +1,68 @@
+﻿using System.Threading.Tasks;
+using AgentFramework.Core.Contracts;
+using AgentFramework.Core.Exceptions;
+using AgentFramework.Core.Messages;
+
+namespace AgentFramework.Core.Decorators.Threading
+{
+    /// <summary>
+    /// Outgoing thread decorator handler
+    /// </summary>
+    public class OutgoingThreadDecoratorHandler : IOutgoingMessageDecoratorHandler
+    {
+        /// <inheritdoc />
+        public string DecoratorIdentifier => "thread";
+
+        /// <inheritdoc />
+        /// <summary>
+        /// Processes adding threading to an outgoing message.
+        /// </summary>
+        /// <param name="message">Outgoing message.</param>
+        /// <param name="agentContext"></param>
+        /// <returns></returns>
+        public Task<OutgoingMessage> ProcessAsync(OutgoingMessage message, IAgentContext agentContext)
+        {
+            //TODO I probably want to be able to set this type of thing from the particular outgoing message, ie only add a certain decorator under a certain condition?
+
+            if (message.OutboundMessage == null)
+                throw new AgentFrameworkException(ErrorCode.InvalidMessage,
+                    "Cannot apply decorator when outbound message is null");
+
+            if (message.InboundMessage == null)
+                return Task.FromResult(message);
+
+            ThreadDecorator previousMessageThreadContext = null;
+            string previousMessageId = null;
+
+            try
+            {
+                previousMessageThreadContext = message.InboundMessage.GetDecorator<ThreadDecorator>(DecoratorIdentifier);
+                previousMessageId = message.InboundMessage.GetMessageId();
+            }
+            catch (AgentFrameworkException) { }
+
+            ThreadDecorator currentThreadContext = null;
+
+            if (previousMessageThreadContext != null)
+            {
+                currentThreadContext = new ThreadDecorator
+                {
+                    ParentThreadId = previousMessageThreadContext.ParentThreadId,
+                    ThreadId = previousMessageThreadContext.ThreadId
+                };
+            }
+            else if (!string.IsNullOrEmpty(previousMessageId))
+            {
+                currentThreadContext = new ThreadDecorator
+                {
+                    ThreadId = previousMessageId
+                };
+            }
+
+            if (currentThreadContext != null)
+                message.OutboundMessage.AddDecorator(currentThreadContext, DecoratorIdentifier);
+
+            return Task.FromResult(message);
+        }
+    }
+}
---
+++ test/AgentFramework.Core.Tests/ThreadingDecoratorTests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Net.Http.Headers;
+using AgentFramework.Core.Decorators.Threading;
+using AgentFramework.Core.Exceptions;
+using AgentFramework.Core.Messages;
+using AgentFramework.Core.Messages.Connections;
+using Xunit;
+
+namespace AgentFramework.Core.Tests
+{
+    public class ThreadingDecoratorTests
+    {
+        [Fact]
+        public void DoesntCreateThreadWhenNoInboundMessage()
+        {
+            var threadDecorator = new OutgoingThreadDecoratorHandler();
+
+            var outgoingMessage = new OutgoingMessage
+            {
+                OutboundMessage = new MessagePayload(new ConnectionRequestMessage())
+            };
+
+            //TODO
+            //threadDecorator.ProcessAsync()
+
+            Assert.Throws<AgentFrameworkException>(() => outgoingMessage.OutboundMessage.GetDecorator<ThreadDecorator>("thread"));
+        }
+
+        [Fact]
+        public void CreatesNewThreadFromUnthreadedInboundMessage()
+        {
+            var threadDecorator = new OutgoingThreadDecoratorHandler();
+
+            var outgoingMessage = new OutgoingMessage
+            {
+                InboundMessage = new MessagePayload(new ConnectionRequestMessage()),
+                OutboundMessage = new MessagePayload(new ConnectionResponseMessage())
+            };
+
+            //TODO
+            //threadDecorator.ProcessAsync()
+
+            var threadingBlock = outgoingMessage.OutboundMessage.GetDecorator<ThreadDecorator>("thread");
+            
+            Assert.True(threadingBlock.ThreadId == outgoingMessage.InboundMessage.GetMessageId());
+            Assert.True(threadingBlock.SenderOrder == 0);
+            Assert.True(threadingBlock.RecievedOrders.Count == 0);
+        }
+
+        [Fact]
+        public void AddsToThreadFromThreadedInboundMessage()
+        {
+            var threadDecorator = new OutgoingThreadDecoratorHandler();
+
+            var threadId = Guid.NewGuid().ToString();
+            var inboundMessage = new MessagePayload(new ConnectionRequestMessage());
+            inboundMessage.AddDecorator(new ThreadDecorator()
+            {
+                ThreadId = threadId
+            }, "thread");
+
+            var outgoingMessage = new OutgoingMessage
+            {
+                InboundMessage = inboundMessage,
+                OutboundMessage = new MessagePayload(new ConnectionResponseMessage())
+            };
+
+            //TODO
+            //threadDecorator.ProcessAsync()
+
+            var threadingBlock = outgoingMessage.OutboundMessage.GetDecorator<ThreadDecorator>("thread");
+
+            Assert.True(threadingBlock.ThreadId == threadId);
+            Assert.True(threadingBlock.SenderOrder == 0);
+            Assert.True(threadingBlock.RecievedOrders.Count == 0);
+        }
+    }
+}
