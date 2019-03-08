3899b891a650a68c9fb7a5728d955a9bbb16f1ab
Tobias Looker <tplooker@gmail.com>
2019-03-08 17:49:06 +1300

    Ephemeral challenge implementation  (#111)
    * Basic working challenge protocol.

---
+++ src/AgentFramework.Core/Models/EphemeralChallenge/CreateChallengeResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AgentFramework.Core.Messages.EphemeralChallenge;
+
+namespace AgentFramework.Core.Models.EphemeralChallenge
+{
+    public class CreateChallengeResult
+    {
+        public string ChallengeId { get; set; }
+
+        public EphemeralChallengeMessage Challenge { get; set; }
+    }
+}
