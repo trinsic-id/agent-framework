e5b2755a924db0d2cfe4ce90481e117a067d0072
Tomislav Markovski <tmarkovski@gmail.com>
2019-03-15 23:34:54 -0400

    Update project to use context. UI reshuffle.

--- samples/aspnetcore/WebAgentMiddleware.cs
+++ samples/aspnetcore/WebAgentMiddleware.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using AgentFramework.AspNetCore.Middleware;
-using AgentFramework.AspNetCore.Options;
-using AgentFramework.Core.Contracts;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Options;
 using WebAgent.Messages;
 using WebAgent.Protocols.BasicMessage;
 
@@ -13,10 +10,8 @@ namespace WebAgent
     {
         public WebAgentMiddleware(
             RequestDelegate next,
-            IWalletService walletService,
-            IServiceProvider serviceProvider,
-            IOptions<WalletOptions> walletOptions)
-            : base(next, walletService, serviceProvider, walletOptions)
+            IServiceProvider serviceProvider)
+            : base(next, serviceProvider)
         {
             AddConnectionHandler();
             AddForwardHandler();
