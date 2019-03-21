fff29e68137af2fb2cf29c4ca27bbe7be9a2f6df
Tobias Looker <tplooker@gmail.com>
2019-03-21 15:36:37 +1300

    Initial test harness commit.

---
+++ test/AgentFramework.TestHarness/ConnectionTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using AgentFramework.Core.Models.Wallets;
+using AgentFramework.TestHarness.Mock;
+using Xunit;
+
+namespace AgentFramework.TestHarness
+{
+    public class ConnectionTests : IAsyncLifetime
+    {
+        WalletConfiguration config1 = new WalletConfiguration { Id = Guid.NewGuid().ToString() };
+        WalletConfiguration config2 = new WalletConfiguration { Id = Guid.NewGuid().ToString() };
+        WalletCredentials cred = new WalletCredentials { Key = "2" };
+
+        private MockAgent _agent1;
+        private MockAgent _agent2;
+        
+        public async Task InitializeAsync()
+        {
+            _agent1 = await MockUtils.CreateAsync(config1, cred);
+            _agent2 = await MockUtils.CreateAsync(config2, cred);
+        }
+
+        [Fact]
+        public async Task ConnectUsingHttp()
+        {
+            await Scenarios.EstablishConnectionAsync(_agent1, _agent2);
+        }
+
+        public async Task DisposeAsync()
+        {
+            await _agent1.Dispose();
+            await _agent2.Dispose();
+        }
+    }
+}
---
+++ test/AgentFramework.TestHarness/Mock/MockAgent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using AgentFramework.Core.Contracts;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AgentFramework.TestHarness.Mock
+{
+    public class MockAgent
+    {
+        public IAgentContext Context { get; set; }
+
+        public IServiceProvider ServiceProvider { get; set; }
+
+        public T GetService<T>() => ServiceProvider.GetRequiredService<T>();
+
+        public async Task Dispose()
+        {
+            Context.Wallet.Dispose();
+            await Context.Wallet.CloseAsync();
+        }
+    }
+}
