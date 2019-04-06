8c94fda5d981953212ce8b785b5ca3385663cc74
Tobias Looker <tplooker@gmail.com>
2019-04-07 01:21:11 +1300

    Improved management of getting the same wallet handle concurrently (#136)

--- test/AgentFramework.Core.Tests/WalletTests.cs
+++ test/AgentFramework.Core.Tests/WalletTests.cs
@@ -10,6 +10,29 @@ namespace AgentFramework.Core.Tests
     public class WalletTests
     {
         [Fact]
+        public async Task ConcurrentWalletAccess()
+        {
+            var walletService = new DefaultWalletService();
+
+            var config = new WalletConfiguration { Id = Guid.NewGuid().ToString() };
+            var creds = new WalletCredentials { Key = "1" };
+
+            await walletService.CreateWalletAsync(config, creds);
+
+            Task<Wallet> openWalletTask1 = walletService.GetWalletAsync(config, creds);
+            Task<Wallet> openWalletTask2 = walletService.GetWalletAsync(config, creds);
+            Task<Wallet> openWalletTask3 = walletService.GetWalletAsync(config, creds);
+            Task<Wallet> openWalletTask4 = walletService.GetWalletAsync(config, creds);
+
+            await Task.WhenAll(openWalletTask1, openWalletTask2, openWalletTask3, openWalletTask4);
+
+            Assert.True(openWalletTask1.Result.IsOpen);
+            Assert.True(openWalletTask2.Result.IsOpen);
+            Assert.True(openWalletTask3.Result.IsOpen);
+            Assert.True(openWalletTask4.Result.IsOpen);
+        }
+
+        [Fact]
         public async Task ProvisionNewWallet()
         {
             var walletService = new DefaultWalletService();
