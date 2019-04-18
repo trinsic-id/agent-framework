45bd4bdff9216397a6de6336922fa38c160ad60c
Tobias Looker <tplooker@gmail.com>
2019-04-19 09:54:24 +1200

    Updates.

--- src/AgentFramework.Core/Models/Credentials/CredentialInfo.cs
+++ src/AgentFramework.Core/Models/Credentials/CredentialInfo.cs
@@ -70,6 +70,6 @@ namespace AgentFramework.Core.Models.Credentials
             $"CredentialDefinitionId={CredentialDefinitionId}, " +
             $"RevocationRegistryId={RevocationRegistryId}, " +
             $"CredentialRevocationId={CredentialRevocationId}, " +
-            $"Attributes={string.Join(",", Attributes ?? new string[0])}";
+            $"Attributes={string.Join(",", Attributes ?? new Dictionary<string,string>())}";
     }
 }
