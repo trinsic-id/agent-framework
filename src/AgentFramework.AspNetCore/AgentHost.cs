280c69e501dcd86269e55036516b2a181bd1d95f
Tomislav Markovski <tmarkovski@gmail.com>
2019-05-11 09:55:08 -0400

    AgentHost for based on generic host pattern

---
+++ src/AgentFramework.AspNetCore/AgentHost.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AgentFramework.Core.Contracts;
+using AgentFramework.Core.Exceptions;
+using AgentFramework.Core.Models.Wallets;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Hosting.Internal;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace AgentFramework.AspNetCore
+{
+    public class AgentHost : IHost
+#if DISPOSE_ASYNC
+      , IAsyncDisposable
+#endif
+    {
+        private readonly ILogger<AgentHost> _logger;
+        private readonly IHostLifetime _hostLifetime;
+        private readonly IProvisioningService _provisioningService;
+        private readonly ApplicationLifetime _applicationLifetime;
+        private readonly HostOptions _options;
+        private IEnumerable<IHostedService> _hostedServices;
+
+        public AgentHost(IServiceProvider services, IApplicationLifetime applicationLifetime, ILogger<AgentHost> logger,
+            IHostLifetime hostLifetime, IOptions<HostOptions> options, IProvisioningService provisioningService)
+        {
+            Services = services ?? throw new ArgumentNullException(nameof(services));
+            _applicationLifetime = (applicationLifetime ?? throw new ArgumentNullException(nameof(applicationLifetime))) as ApplicationLifetime;
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _hostLifetime = hostLifetime ?? throw new ArgumentNullException(nameof(hostLifetime));
+            _provisioningService = provisioningService;
+            _options = options?.Value ?? throw new ArgumentNullException(nameof(options));
+        }
+
+        public IServiceProvider Services { get; }
+
+        public async Task StartAsync(CancellationToken cancellationToken = default)
+        {
+            //_logger.Starting();
+
+            await _hostLifetime.WaitForStartAsync(cancellationToken);
+
+            cancellationToken.ThrowIfCancellationRequested();
+            _hostedServices = Services.GetService<IEnumerable<IHostedService>>();
+
+            foreach (var hostedService in _hostedServices)
+            {
+                // Fire IHostedService.Start
+                await hostedService.StartAsync(cancellationToken).ConfigureAwait(false);
+            }
+
+            try
+            {
+                // Provision agent on service startup
+                await _provisioningService.ProvisionAgentAsync(Services.GetService<ProvisioningConfiguration>());
+            }
+            catch (AgentFrameworkException ex) when (ex.ErrorCode == ErrorCode.WalletAlreadyProvisioned)
+            {
+                // Agent provisioned. Continue.
+            }
+
+            // Fire IHostApplicationLifetime.Started
+            _applicationLifetime?.NotifyStarted();
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken = default)
+        {
+            using (var cts = new CancellationTokenSource(_options.ShutdownTimeout))
+            using (var linkedCts = CancellationTokenSource.CreateLinkedTokenSource(cts.Token, cancellationToken))
+            {
+                var token = linkedCts.Token;
+                // Trigger IHostApplicationLifetime.ApplicationStopping
+                _applicationLifetime?.StopApplication();
+
+                IList<Exception> exceptions = new List<Exception>();
+                if (_hostedServices != null) // Started?
+                {
+                    foreach (var hostedService in _hostedServices.Reverse())
+                    {
+                        token.ThrowIfCancellationRequested();
+                        try
+                        {
+                            await hostedService.StopAsync(token).ConfigureAwait(false);
+                        }
+                        catch (Exception ex)
+                        {
+                            exceptions.Add(ex);
+                        }
+                    }
+                }
+
+                token.ThrowIfCancellationRequested();
+                await _hostLifetime.StopAsync(token);
+
+                // Fire IHostApplicationLifetime.Stopped
+                _applicationLifetime?.NotifyStopped();
+
+                if (exceptions.Count > 0)
+                {
+                    var ex = new AggregateException("One or more hosted services failed to stop.", exceptions);
+                    throw ex;
+                }
+            }
+        }
+
+#if DISPOSE_ASYNC
+        public void Dispose()
+        {
+            DisposeAsync().GetAwaiter().GetResult();
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            switch (Services)
+            {
+                case IAsyncDisposable asyncDisposable:
+                    await asyncDisposable.DisposeAsync();
+                    break;
+                case IDisposable disposable:
+                    disposable.Dispose();
+                    break;
+            }
+        }
+#else
+        public void Dispose()
+        {
+            (Services as IDisposable)?.Dispose();
+        }
+#endif
+    }
+}
