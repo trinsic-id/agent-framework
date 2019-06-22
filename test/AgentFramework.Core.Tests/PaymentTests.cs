b7a7c343d12b7e67e4b788cf35e70994bf275c69
Tomislav Markovski <tmarkovski@gmail.com>
2019-06-22 16:39:00 -0400

    Mint tokens unit test

--- test/AgentFramework.Core.Tests/PaymentTests.cs
+++ test/AgentFramework.Core.Tests/PaymentTests.cs
@@ -5,10 +5,14 @@ using AgentFramework.Core.Handlers;
 using AgentFramework.Payments.Abstractions;
 using AgentFramework.Payments.SovrinToken;
 using AgentFramework.TestHarness.Utils;
+using Hyperledger.Indy.DidApi;
 using Hyperledger.Indy.WalletApi;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Xunit;
+using AgentFramework.Core.Extensions;
+using Indy = Hyperledger.Indy.PaymentsApi;
+using Hyperledger.Indy.LedgerApi;
 
 namespace AgentFramework.Core.Tests
 {
@@ -48,14 +52,32 @@ namespace AgentFramework.Core.Tests
 
             Assert.NotNull(address);
             Assert.NotNull(address.Address);
+        }
 
+        [Fact(DisplayName = "Mint Sovrin tokens")]
+        public async Task MintSovrinTokens()
+        {
             try
             {
+                var paymentService = _host.Services.GetService<IPaymentService>();
+                var address = await paymentService.CreatePaymentAddressAsync(_context);
 
+                var trustee1 = await Did.CreateAndStoreMyDidAsync(_context.Wallet, new { seed = "000000000000000000000000Trustee1" }.ToJson());
+                var trustee2 = await Did.CreateAndStoreMyDidAsync(_context.Wallet, new { seed = "000000000000000000000000Trustee2" }.ToJson());
+                var trustee3 = await Did.CreateAndStoreMyDidAsync(_context.Wallet, new { seed = "000000000000000000000000Trustee3" }.ToJson());
+
+                var request = await Indy.Payments.BuildMintRequestAsync(_context.Wallet, trustee1.Did, new[] { new { recipient = address.Address, amount = 1000 } }.ToJson(), null);
+                var singedRequest = await Ledger.MultiSignRequestAsync(_context.Wallet, trustee1.Did, request.Result);
+                singedRequest = await Ledger.MultiSignRequestAsync(_context.Wallet, trustee2.Did, singedRequest);
+                singedRequest = await Ledger.MultiSignRequestAsync(_context.Wallet, trustee3.Did, singedRequest);
+
+                var response = await Ledger.SubmitRequestAsync(await _context.Pool, singedRequest);
+
+                Console.WriteLine(response);
             }
-            catch (Exception e)
+            catch(Exception e)
             {
-
+                Console.WriteLine(e);
             }
         }
     }
